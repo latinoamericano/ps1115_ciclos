8212d14229d024cffbeb50336bd023f5570c66fe
diegoacuario <diegoacuario11@gmail.com>
2015-03-21 11:56:51 -0500

    Version inicio

--- src/FuncionesCiclos.java
+++ src/FuncionesCiclos.java
@@ -5,4 +5,9 @@
  */
 public class FuncionesCiclos {
 
+    public void test(int x) {
+        for (int i = 0; i < x; i++) {
+            System.out.println((int)(10+(Math.random()*90)));
+        }
+    }
 }
--- src/TestCiclos.java
+++ src/TestCiclos.java
@@ -1,9 +1,13 @@
+
 /**
  *
  * @author DIEGO
  */
 public class TestCiclos {
+
     public static void main(String[] args) {
-        
+        FuncionesCiclos f = new FuncionesCiclos();
+        int y = 10;
+        f.test(y);
     }
 }
